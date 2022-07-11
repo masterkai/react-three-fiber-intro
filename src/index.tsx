47c7e7ff74940197318b6afd41cf3d0bf407aec9
masterkai <kai0919@hotmail.com>
2022-07-12 07:42:52 +0800

    add: react three fiber

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
+// @ts-ignore
 import React from 'react';
+// @ts-ignore
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// @ts-ignore
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
